42d77a2bad5eb0c03dca8267c14c44a7082223ea
sachse_t <Thomas.Sachse@ottogroup.com>
2015-12-30 15:24:29 +0100

    ..

--- .docker_run.sh
+++ .docker_run.sh
@@ -1,3 +1,3 @@
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run -v $DIR:/wiki -p 8759:80 personalwiki 
+docker run -d -v $DIR:/wiki -p 8759:80 --name personalwiki personalwiki
 
